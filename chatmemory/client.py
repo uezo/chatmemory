da4e0216614b26d8eac8d9b06a5c01c7ee6c073b
uezo <uezo@uezo.net>
2023-09-11 08:16:26 +0900

    Make it possible to customize client grounding template
    ChatMemoryClient
    
    - archive_template
    - entities_template

--- chatmemory/client.py
+++ chatmemory/client.py
@@ -2,9 +2,29 @@ import json
 import requests
 
 class ChatMemoryClient:
-    def __init__(self, url: str="http://127.0.0.1:8123", archive_injection_at: int=2):
+    ARCHIVE_TEMPLATE = """以下はここ5日間にあったユーザーとの会話を要約したものです。
+```
+{archives_str}
+```
+
+基本的にこの会話の情報を利用する必要はありませんが、会話の流れでこれらの情報が必要になった場合は利用してください。
+"""
+    ENTITIES_TEMPLATE = """
+
+# ユーザーに関して会話を通じて聞き出したこと
+
+以下はユーザーとの会話を通じてあなたが記憶している内容です。強く意識する必要はありませんが、会話の流れでこれらの情報が必要になった場合はこれらの情報を会話に利用してください。
+
+```
+{entities_str}
+```
+"""
+
+    def __init__(self, url: str="http://127.0.0.1:8123", archive_injection_at: int=2, archive_template: str=ARCHIVE_TEMPLATE, entities_template: str=ENTITIES_TEMPLATE):
         self.url = url
         self.archive_injection_at = archive_injection_at
+        self.archive_template = archive_template
+        self.entities_template = entities_template
 
     def add_histories(self, user_id: str, messages: list):
         requests.post(f"{self.url}/histories/{user_id}", json={"messages": messages})
@@ -15,17 +35,9 @@ class ChatMemoryClient:
     def get_archived_histories_content(self, user_id: str) -> str:
         archives = self.get_archived_histories(user_id)
         if archives:
-            archives_str = ""
-            for a in archives:
-                archives_str += f'- {a["date"]}: {a["archive"]}\n'
-
-            return f"""以下はここ3日間にあったユーザーとの会話を要約したものです。
-```
-{archives_str}
-```
-
-基本的にこの会話の情報を利用する必要はありませんが、会話の流れでこれらの情報が必要になった場合は利用してください。
-"""
+            return self.archive_template.format(
+                archives_str="\n".join([f'- {a["date"]}: {a["archive"]}' for a in archives])
+            )
 
         return ""
 
@@ -50,18 +62,10 @@ class ChatMemoryClient:
 
     def get_entities_content(self, user_id: str) -> str:
         entities = self.get_entities(user_id)
-
         if entities:
-            return f"""
-
-# ユーザーに関して会話を通じて聞き出したこと
-
-以下はユーザーとの会話を通じてあなたが記憶している内容です。強く意識する必要はありませんが、会話の流れでこれらの情報が必要になった場合はこれらの情報を会話に利用してください。
-
-```
-{json.dumps(entities, ensure_ascii=False, indent=2)}
-```
-"""
+            return self.entities_template.format(
+                entities_str="\n".join([f"- {k}: {v}" for k, v in entities.items()])
+            )
         
         return ""
 
