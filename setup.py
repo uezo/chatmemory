dd1d12d722cca3b0b97c73dac30c96dc1eedcc14
f6844710 <127287746+f6844710@users.noreply.github.com>
2024-02-13 00:04:41 +0900

    v0.1.4✨

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="Long-term and medium-term memories between you and chatbot💕",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=["fastapi==0.100.0", "openai==0.27.8", "requests==2.31.0", "SQLAlchemy==2.0.20", "uvicorn==0.23.1", "pycryptodome==3.18.0"],
+    install_requires=["fastapi==0.100.0", "openai==1.1.0", "requests==2.31.0", "SQLAlchemy==2.0.20", "uvicorn==0.23.1", "pycryptodome==3.18.0"],
     license="Apache v2",
     packages=["chatmemory"],
     classifiers=[
