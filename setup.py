4cd2d8800be886fdb9bc06f13b7d826ecd88f953
uezo <uezo@uezo.net>
2025-12-06 20:55:56 +0900

    Update for v0.2.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="chatmemory",
-    version="0.2.4",
+    version="0.2.6",
     url="https://github.com/uezo/chatmemory",
     author="uezo",
     author_email="uezo@uezo.net",
@@ -11,7 +11,7 @@ setup(
     description="The simple yet powerful long-term memory manager between AI and you💕",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=["fastapi==0.115.8", "openai==1.64.0", "uvicorn==0.34.0", "psycopg2-binary==2.9.10"],
+    install_requires=["fastapi>=0.115.8", "openai>=1.64.0", "uvicorn>=0.34.0", "psycopg2-binary>=2.9.10"],
     license="Apache v2",
     packages=["chatmemory"],
     classifiers=[
